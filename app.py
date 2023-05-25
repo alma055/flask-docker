94dd12b7617e775b39329334c8fb2644667a810d
Alma Mata <almamata115@gmail.com>
2023-05-25 02:06:27 -0600

    -

--- app.py
+++ app.py
@@ -1,53 +1,181 @@
-from flask import Flask, render_template, make_response
+from flask import Flask, render_template, make_response, g, request, session, flash, redirect, url_for
+import json
 from apps.manejo_archivos import *
 import os
 import random
+import sys
 
 app = Flask(__name__)
-@app.route('/', methods=['POST','GET'])
-def login():
-    return render_template("pages-login.html")
+
+app.config['SECRET_KEY'] = 'super_secret_key'
+app.config['SESSION_TYPE'] = 'filesystem'
+
+app.config['lista_partidos'] = abrir_archivo("db/partidos_politicos.csv")
+app.config['usuarios'] = abrir_archivo("db/users.csv")
+app.config['preguntas'] = abrir_archivo("db/cuestionario.csv")
+
+def verificar_sesion():
+    # if session.get('logged_in'):
+    #     if session['logged_in']:
+    #         return redirect(url_for('Inicio'))
+    # if session.get('logged_in') == True:
+    #     return True
+    # return False
+        # return redirect(url_for('Inicio'))
+    #print(session['usuario'])
+    if 'usuario' not in session:
+        return True
+        # return redirect(url_for('Inicio'))
+    pass
+
+def ini_var_session(usuario):
+    session['usuario'] = usuario['username']
+    session['password'] = usuario['password']
+    session['name'] = usuario['Name']
+    session['email'] = usuario['email']
+    session['punteo'] = usuario['punteo_cuestionario']
+    session['recomendacion'] = usuario['recomendacion']
+    session['otras'] = usuario['otras_recomendaciones']
+    session['logged_in'] = True
+
+def end_var_session():
+    session.pop('usuario', None)
+    session.pop('password', None)
+    session.pop('name', None)
+    session.pop('email', None)
+    session.pop('punteo', None)
+    session.pop('recomendacion', None)
+    session.pop('otras', None)
+    session.pop('logged_in', None)
+    return redirect(url_for('Inicio'))
+
+@app.before_request
+def antes_de_abrir():
+    g.lista_partidos = app.config['lista_partidos']
+    g.usuarios = app.config['usuarios']
+    g.preguntas = app.config['preguntas']
+
+@app.route('/', methods=['POST', 'GET'])
+def Inicio():
+    if 'usuario' in session:
+        return redirect(url_for('pagina_inicial'))
+    if request.method == 'POST':
+        usuario = request.form['username']
+        psw = request.form['password']
+        autenticacion_usuario = autenticacion(g.usuarios, usuario, psw)
+        if autenticacion_usuario[0] == 'False':
+            flash('El usuario o la contrasenia no coinciden')
+            return render_template('pages-login.html')
+        ini_var_session(autenticacion_usuario[1])
+        return redirect(url_for('pagina_inicial'))
+    return render_template('pages-login.html')
 
 @app.route('/pages-register',methods=['POST', 'GET'])
 def crear_cuenta():
+    if 'usuario' in session:
+        return redirect(url_for('pagina_inicial'))
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        name = request.form['name']
+        email = request.form['email']
+        if usuario_existe(g.usuarios, username):
+            flash('El usuario ya existe')
+            return render_template('pages-register.html')
+        registro = {"username":username, "Name":name, "email":email, "password":password, "punteo_cuestionario": '', "recomendacion":'', "otras_recomendaciones":''}
+        g.usuarios.append(registro)
+        guardar_archivo("db/users.csv",g.usuarios)
+        return render_template('pages-login.html')
     return render_template('pages-register.html')
 
 @app.route('/index', methods=['POST','GET'])
 def pagina_inicial():
-    lista_partidos = abrir_archivo("db/partidos_politicos.csv")
-    return render_template('index.html', partidos=lista_partidos)
+    if verificar_sesion():
+        return redirect(url_for('Inicio'))
+    else:
+        datos = graficas('db/PartidosAfiliados.csv')
+        valores_json = json.dumps(datos[0])
+        categorias_json = json.dumps(datos[1])
+        if session['recomendacion'] != '':
+            return render_template('index.html', partidos=g.lista_partidos, resultado=[int(num) for num in session['recomendacion'].split(",")], valores = valores_json, categorias = categorias_json)
+        else:
+            return render_template('index.html', partidos=g.lista_partidos, valores = valores_json, categorias = categorias_json)
 
 @app.route('/banco-informacion', methods=['POST','GET'])
 def banco_informacion_general():
-    lista_partidos = abrir_archivo("db/partidos_politicos.csv")
-    return render_template('banco_informacion.html',datos = random.sample(lista_partidos, len(lista_partidos)), partidos=lista_partidos)
+    if verificar_sesion():
+        return redirect(url_for('Inicio'))
+    else:
+        return render_template('banco_informacion.html',datos = random.sample(g.lista_partidos, len(g.lista_partidos)), partidos=g.lista_partidos)
 
 @app.route('/banco-informacion/<partido>', methods=['POST','GET'])
 def informacion_partido(partido):
-    lista_partidos = abrir_archivo("db/partidos_politicos.csv")
-    especifico = buscar_dato(lista_partidos, partido)
-    return render_template('partido-politico.html', partidos=lista_partidos, especifico=especifico)
+    if verificar_sesion():
+        return redirect(url_for('Inicio'))
+    else:
+        especifico = buscar_dato(g.lista_partidos, partido, "siglas")
+        return render_template('partido-politico.html', partidos=g.lista_partidos, especifico=especifico)
 
-@app.route('/users-profile',methods=['POST', 'GET'])
-def info_usuario():
-    lista_partidos = abrir_archivo("db/partidos_politicos.csv")
-    return render_template('users-profile.html', partidos=lista_partidos)
+@app.route('/cuestionario', methods=['POST','GET'])
+def cuestionario():
+    if verificar_sesion():
+        return redirect(url_for('Inicio'))
+    else:
+        return render_template('cuestionario.html', partidos=g.lista_partidos, preguntas=g.preguntas)
+@app.route('/procesar-formulario', methods=['POST'])
+def procesar_formulario():
+    if verificar_sesion():
+        return redirect(url_for('Inicio'))
+    else:
+        respuestas = {}
+        for pregunta in g.preguntas:
+            respuesta_key = f'p{g.preguntas.index(pregunta)+1}-{g.preguntas.index(pregunta)+1}'
+            respuesta = request.form.get(respuesta_key, '')
+            if respuesta == '':
+                flash('Por favor responda todas las preguntas...')
+                return redirect(url_for('cuestionario'))
+            respuestas[respuesta_key] = respuesta
+        #print(respuestas)
+        registro = buscar_dato(g.usuarios, session['usuario'], 'username')
+        indice = g.usuarios.index(registro)
+        punteo = calcular_punteo(respuestas)
+        recomendacion,otras = recomendar('db/partidos_politicos.csv', punteo)
+        session['recomendacion'] = recomendacion
+        g.usuarios[indice] = {"username":session['usuario'], "Name":session['name'], "email":session['email'], "password":session['password'], "punteo_cuestionario": punteo, "recomendacion":recomendacion, "otras_recomendaciones":otras}
+        guardar_archivo("db/users.csv",g.usuarios)
+        return redirect(url_for('resultado'))
 
-@app.route('/pages-faq',methods=['POST', 'GET'])
-def pagina_faq():
-    lista_partidos = abrir_archivo("db/partidos_politicos.csv")
-    return render_template('pages-faq.html', partidos=lista_partidos)
-
-@app.route('/pages-contact',methods=['POST', 'GET'])
-def pagina_contactanos():
-    lista_partidos = abrir_archivo("db/partidos_politicos.csv")
-    return render_template('pages-contact.html', partidos=lista_partidos)
-
+@app.route('/resultado', methods=['GET'])
+def resultado():
+    if verificar_sesion():
+        return redirect(url_for('Inicio'))
+    else:
+        return render_template('resultado.html', partidos=g.lista_partidos, resultado=[int(num) for num in session['recomendacion'].split(",")])
+    
+@app.route('/popularidad', methods=['GET'])
+def popularidad():
+    if verificar_sesion():
+        return redirect(url_for('Inicio'))
+    else:
+        datos = graficas('db/PartidosAfiliados.csv')
+        valores_json = json.dumps(datos[0])
+        categorias_json = json.dumps(datos[1])
+        return render_template('index.html', partidos=g.lista_partidos, valores = valores_json, categorias = categorias_json)
+    
+@app.route('/otras-recomendacion', methods=['GET'])
+def otros():
+    if verificar_sesion():
+        return redirect(url_for('Inicio'))
+    else:
+        return render_template('resultado.html', partidos=g.lista_partidos, resultado=[int(num) for num in session['otras'].split(",")])
 @app.errorhandler(404)
 def not_found(error):
     """Page not found."""
     return make_response(render_template("pages-error-404.html"), 404)
-
+@app.route('/logout')
+def logout():
+    end_var_session()
+    return redirect("/")
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
     app.run(debug=True, host='0.0.0.0', port=port) 
--- apps/manejo_archivos.py
+++ apps/manejo_archivos.py
@@ -1,5 +1,6 @@
 import csv
-columnas = []
+import pandas as pd
+columnas_usuarios = ["username","Name","email","password","punteo_cuestionario","recomendacion","otras_recomendaciones"]
 def abrir_archivo(nombre_archivo):
     registros = []
     with open(nombre_archivo, "r", encoding="utf8") as archivo_csv:
@@ -8,16 +9,68 @@ def abrir_archivo(nombre_archivo):
             registros.append(fila)
     return registros
 
-def buscar_dato(lista, dato_buscar):
+def buscar_dato(lista, dato_buscar,dato_busqueda):
     resultado = 0
     for dato in lista:
-        if dato['siglas'] == (dato_buscar):
+        if dato[dato_busqueda] == (dato_buscar):
             resultado = dato
     return resultado
 
 def guardar_archivo(nombre_archivo, lista):
     with open(nombre_archivo, "w", encoding='utf8', newline='') as archivo_csv:
-        escrito = csv.DictWriter(archivo_csv, fieldnames=columnas)
+        escrito = csv.DictWriter(archivo_csv, fieldnames=columnas_usuarios)
         escrito.writeheader()
-        for pelicula in lista:
-            escrito.writerow(pelicula)
+        for dato in lista:
+            escrito.writerow(dato)
+
+def usuario_existe(lista_usuarios, user):
+    for usuario in lista_usuarios:
+        if usuario['username'] == user:
+            return True
+    return False
+
+def autenticacion(lista,usuario, psw):
+    for user in lista:
+        if user['username'] == usuario and user['password'] == psw:
+            return ["True", user]
+    return ["False",{}]
+
+def calcular_punteo(respuestas):
+    resultado = 0
+    for respuesta in respuestas:
+        lista = respuestas[respuesta].split(",")
+        resultado += float(lista[0])*float(lista[1])
+    return resultado
+def posicion(resultado):
+    posicion = ''
+    if resultado > 0 and resultado <= 5:
+        posicion = 'Centro'
+    elif resultado > 5 and resultado <= 15:
+        posicion = 'Centroderecha'
+    elif resultado > 15:
+        posicion = 'Derecha'
+    elif resultado < 0 and resultado >= -3:
+        posicion = 'Centro'
+    elif resultado < -3 and resultado >= -12:
+        posicion = 'Centroizquierda'
+    elif resultado <-12:
+        posicion = 'Izquierda'
+    return posicion
+
+def recomendar(nombre_archivo, resultado):
+    pos = posicion(resultado)
+    lista_partidos = pd.read_csv(nombre_archivo)
+    partidos = lista_partidos.loc[lista_partidos['posicion_politica'] == pos]
+    respuesta = partidos.iloc[0:3]
+    otras = partidos.iloc[3:]
+    respuesta_string = list(respuesta['id_partido'])
+    respuesta2 = list(otras['id_partido'])
+    final = ",".join(str(e) for e in respuesta_string)
+    final2 = ",".join(str(e) for e in respuesta2)
+    return final,final2
+
+def graficas(nombre_archivo):
+    datos = pd.read_csv(nombre_archivo)
+    valores = datos['afiliados'].tolist()
+    categorias = datos['siglas'].tolist()
+    return [valores,categorias]
